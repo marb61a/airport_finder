16ba2006f87ac980c77231847c34989cb3f96abb
marb <marb61a@gmail.com>
2015-10-20 08:09:49 +0000

    updated controller and added indecies to mongo

--- client/controllers/AppController.js
+++ client/controllers/AppController.js
@@ -8,4 +8,25 @@ myApp.controller('AppController', ['$scope','$http', function($scope, $http){
 			$scope.airports = response;
 		});
 	};
+	
+	$scope.findAirports = function(){
+		$http.get('/api/airports/state/'+$scope.stateCode).success(function(response){
+			$scope.airports = response;
+		});
+	};
+
+	$scope.findAirportsByProx = function(){
+		var location = {
+			distance: $scope.location.distance
+		};
+		$http.get('/geocode/location?address='+$scope.location.address).success(function(loc_response){
+			location.lat = loc_response.locations[0].latitude;
+			location.lon = loc_response.locations[0].longitude;
+			
+			$http.post('/api/airports/prox', location).success(function(response){
+				$scope.airports = response;
+			});
+		});
+	};
+	
 }]);
